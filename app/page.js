e67a25359ea0741093d518cb9d74804de3df34c0
Micheal Callaghan <mehaul96@gmail.com>
2025-03-11 12:09:17 -0400

    initial commit

--- app/page.js
+++ app/page.js
@@ -15,9 +15,9 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.js</code>.
+            Something basic in Next JS<code>by Micheal Callaghan lol</code>.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>SBuilding out a template</li>
         </ol>
 
         <div className={styles.ctas}>
