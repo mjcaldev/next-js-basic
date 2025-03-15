cec2ce314cdfd6d2c8d18837febcdd5cdda19189
Micheal Callaghan <mehaul96@gmail.com>
2025-03-14 21:53:18 -0400

    added try catch to throw 404 if API error occurs

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,11 +1,25 @@
+import { notFound } from "next/navigation";
+
+async function fetchPosts(id) {
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      next: {
+        revalidate: 30,
+      },
+    });
+    const data = await response.json(); 
+    return data;
+  } catch (err) {
+    notFound(); 
+  }
+  
+}
+
 export default async function Page({ params }) {
   const {id} = params;
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    next: {
-      revalidate: 30,
-    },
-  });
-  const data = await response.json();
+
+  const data = await fetchPosts(id);
+
   return (
     <main>
       <h1>Individual page: {id}</h1>
@@ -13,3 +27,13 @@ export default async function Page({ params }) {
     </main>
   );
 }
+
+export async function generateStaticParams() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data.map((post) => ({
+    id: String(post.id),
+  }))
+};
+
+export const dynamicParams = false;
